b2e5f60609f02f86a156bedade8e99b7c1b92f0e
allanwzhang <33790915+allanwzhang@users.noreply.github.com>
2020-07-02 14:07:36 -0400

    Add files via upload
    Basis of user input, combined streets together. Stored as HashMap (Street name -> all Streets segments).

--- streetview.java
+++ streetview.java
@@ -4,94 +4,94 @@ import java.net.*;
 import java.nio.file.*;
 
 public class streetview {
+	static double totalImages = 0;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("Centerline.csv"));
-		/*
-		ArrayList<Street> streets = parseData(f);
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Metadata.txt")));
 		
+		TreeMap<String, ArrayList<Street>> streets = parseData(f);
 		findHeading(streets);
+		
 		System.out.println("Time: " + System.currentTimeMillis());
-		System.out.println(streets.get(83).name);
-		for(double[] arr : streets.get(83).points) {
-			System.out.println(arr[0] + " " + arr[1]);
+		for(String key : streets.keySet()) {
+			System.out.println(key);
 		}
-		System.out.println(streets.get(83).avDist);
-		System.out.println(streets.get(83).fov);
-		System.out.println(streets.get(83).width);
-		*/
 		
-		TreeCalc t = new TreeCalc(40.76050360780349, -73.96022079083882, 40.76052727, -73.96014289, 28.0);
-		System.out.println(t.xy[0] + " " + t.xy[1]);
-		System.out.println(t.imageY);
+		System.out.println("Give me a street from the list");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine().toUpperCase();
+		while(!streets.containsKey(input)) {
+			System.out.println("That isn't a valid street");
+			System.out.println("Please give me one from the list");
+			input = sc.nextLine();
+		}
+		for(Street curr : streets.get(input)) {
+			System.out.println(curr);
+			callAPI(curr, out);
+		}
 		
-//		callAPI(streets.get(83));
+		System.out.println(totalImages);
 		
+		sc.close();
+		out.close();
 		f.close();
 	}
 	
-	static void callAPI(Street s) throws IOException {
-		int count = 0;
+	static void callAPI(Street s, PrintWriter out) throws IOException {	
 		for(int i = 0; i < s.points.size(); i++) {
 			double[] curr = s.points.get(i);
 			String url = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location=" + curr[0] + "," + curr[1] + 
-						 "&fov=" + s.fov + "&heading=" + curr[2] + "&pitch=0&key=AIzaSyCKwmE_1aUZLozFXho7Vp9B9rQY4xUkRqU";
+						 "&fov=" + s.fov + "&heading=" + curr[2] + "&pitch=0&source=outdoor&key=AIzaSyCi7VDbFboRwENnMCmE2LuEf5kyuwEnBwU";
 			try(InputStream in = new URL(url).openStream()){
-			    Files.copy(in, Paths.get("C:/Allan/Streetview/img" + count + ".jpg"));
+			    Files.copy(in, Paths.get("C:/Allan/Streetview/" + s.name.replace(' ', '_') + "_s1_" + totalImages + ".jpg"));
 			}
+			out.println(s.name.replace(' ', '_') + "_s1_" + totalImages + ".jpg");
+			out.println(url);
 			url = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location=" + curr[0] + "," + curr[1] + 
-				  "&fov=" + s.fov + "&heading=" + (curr[2] + 180) + "&pitch=0&key=AIzaSyCKwmE_1aUZLozFXho7Vp9B9rQY4xUkRqU";
-			
+				  "&fov=" + s.fov + "&heading=" + (curr[2] + 180) + "&pitch=0&key=AIzaSyCi7VDbFboRwENnMCmE2LuEf5kyuwEnBwU";
 			try(InputStream in = new URL(url).openStream()){
-				Files.copy(in, Paths.get("C:/Allan/Streetview/img" + (count + 1)+ ".jpg"));
+				Files.copy(in, Paths.get("C:/Allan/Streetview/" + s.name.replace(' ', '_')  + "_s2_"+ (totalImages + 1) + ".jpg"));
 			}
-			count += 2;
+			out.println(s.name.replace(' ', '_') + "_s2_" + (totalImages + 1) + ".jpg");
+			out.println(url);
+			totalImages += 2;
 		}
+		
 	}
 	
-	static void findHeading(ArrayList<Street> streets) {
-		for(Street s : streets) {
-			for(int i = 1; i < s.points.size(); i++) {
-				double[] left = s.points.get(i - 1);
-				double[] right = s.points.get(i);
-				Deg2UTM leftutm = new Deg2UTM(left[0], left[1]);
-				Deg2UTM rightutm = new Deg2UTM(right[0], right[1]);
-				double slope = -1 *  (rightutm.Northing - leftutm.Northing) / (rightutm.Easting - leftutm.Easting);
-				slope = 1 / slope;
-				double degrees = 90 - Math.toDegrees(Math.atan(slope));
-				s.points.get(i - 1)[2] = Math.round(degrees);
-				s.points.get(i)[2] = Math.round(degrees);
-			}
-		}
-	}
-	
-	static ArrayList<Street> parseData(BufferedReader f) throws IOException {
-		ArrayList<Street> streets = new ArrayList<>();
+	static TreeMap<String, ArrayList<Street>> parseData(BufferedReader f) throws IOException {
+		TreeMap<String, ArrayList<Street>> streets = new TreeMap<>();
 		f.readLine();
 		System.out.println("Time: " + System.currentTimeMillis());
 		while(f.ready()) {
 			ArrayList<String> curr = split(f.readLine());
 			int type = Integer.parseInt(curr.get(18));
-			if(type != 1 || Integer.parseInt(curr.get(14)) == 0) continue;
-			Street toAdd = new Street(findPoints(curr.get(3)), curr.get(10), Integer.parseInt(curr.get(14)) / 3.281);
+			if(type != 1 || Integer.parseInt(curr.get(14)) == 0 || Integer.parseInt(curr.get(13)) != 1) continue;
+			Street toAdd = new Street(findPoints(curr.get(3)), removeEW(curr.get(10)), Integer.parseInt(curr.get(14)) / 3.281);
 			toAdd.avDist = findAvDist(toAdd.points);
 			toAdd.fov = calculateFOV(toAdd);
-			while(toAdd.fov > 100) {
+			while(toAdd.fov > 60) {
 				doublePoints(toAdd.points);
 				toAdd.avDist = findAvDist(toAdd.points);
 				toAdd.fov = calculateFOV(toAdd);
 			}
-			streets.add(toAdd);
+			if(streets.containsKey(toAdd.name)) {
+				streets.get(toAdd.name).add(toAdd);
+			} else {
+				ArrayList<Street> newStreet = new ArrayList<>();
+				newStreet.add(toAdd);
+				streets.put(toAdd.name, newStreet);
+			}
 		}
 		System.out.println("Time: " + System.currentTimeMillis());
 		return streets;
 	}
 	
-	static void doublePoints(ArrayList<double[]> points) {
-		for(int i = 1; i < points.size(); i++) {
-			points.add(i, mid(points.get(i - 1)[0], points.get(i - 1)[1], points.get(i)[0], points.get(i)[1]));
-			i++;
-		}
+	static String removeEW(String name) {
+		if(name.split(" ").length < 2) return name;
+		if(name.split(" ")[0].equals("E") || name.split(" ")[0].equals("W")) return name.substring(2);
+		return name;
 	}
 	
 	static int calculateFOV(Street s) {
@@ -104,6 +104,24 @@ public class streetview {
 			sum += FlatEarthDist.distance(points.get(i - 1)[0], points.get(i - 1)[1], points.get(i)[0], points.get(i)[1]);
 		}
 		return sum / (points.size() - 1);
+	}
+	
+	static void findHeading(TreeMap<String, ArrayList<Street>> streets) {
+		for(String key : streets.keySet()) {
+			for(Street s : streets.get(key)) {
+				for(int i = 1; i < s.points.size(); i++) {
+					double[] left = s.points.get(i - 1);
+					double[] right = s.points.get(i);
+					Deg2UTM leftutm = new Deg2UTM(left[0], left[1]);
+					Deg2UTM rightutm = new Deg2UTM(right[0], right[1]);
+					double slope = -1 *  (rightutm.Northing - leftutm.Northing) / (rightutm.Easting - leftutm.Easting);
+					slope = 1 / slope;
+					double degrees = 90 - Math.toDegrees(Math.atan(slope));
+					s.points.get(i - 1)[2] = Math.round(degrees);
+					s.points.get(i)[2] = Math.round(degrees);
+				}
+			}
+		}
 	}
 	
 	static ArrayList<double[]> findPoints(String s) {
@@ -119,6 +137,13 @@ public class streetview {
 		return result;
 	}
 
+	static void doublePoints(ArrayList<double[]> points) {
+		for(int i = 1; i < points.size(); i++) {
+			points.add(i, mid(points.get(i - 1)[0], points.get(i - 1)[1], points.get(i)[0], points.get(i)[1]));
+			i++;
+		}
+	}
+	
 	static double[] mid(double lat1, double lon1, double lat2, double lon2){
 
 	    double dLon = Math.toRadians(lon2 - lon1);
@@ -134,6 +159,12 @@ public class streetview {
 	    double lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
 
 	    return new double[] {Math.toDegrees(lat3), Math.toDegrees(lon3), -1};
+	}
+	
+	static String printURL(Street s, int j) {
+		String result = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location=" + s.points.get(j)[0] + ","
+						+ s.points.get(j)[1] + "&fov=" + s.fov + "&heading=" + s.points.get(j)[2] + "&pitch=0&source=outdoor&key=AIzaSyCi7VDbFboRwENnMCmE2LuEf5kyuwEnBwU";
+		return result;
 	}
 	
 	static ArrayList<String> split(String s) {
